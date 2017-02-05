e3d266e53948b3741c88fe0072765ba553944a1d
Oliver Marks <oly@digitaloctave.com>
2017-02-05 13:58:30 +0000

    add gunicorn logging

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilescss
-/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/gunicorn.sock --chdir=/app
+/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/gunicorn.sock --error-logfile /var/log/gunicorn/gunicorn-error.log --chdir=/app
