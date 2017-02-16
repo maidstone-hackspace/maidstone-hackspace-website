e0727c03f53f7f82923d01bf3eaa991f521bd82e
Sam Collins <sam@scollins.co>
2017-02-16 23:19:50 +0000

    Added Blog page and views

--- mhackspace/blog/views.py
+++ mhackspace/blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from mhackspace.blog.models import Post, Category
 
-# Create your views here.
+def blog(request, slug=None, category=None):
+    categories = Category.objects.all()
+
+    try:
+        if slug is not None:
+            blog_posts = Post.objects.filter(slug=slug)
+        elif category is not None:
+            category = Category.objects.filter(slug=category)
+            blog_posts = Post.objects.filter(categories=category)
+        else:
+            blog_posts = Post.objects.all()
+    except Category.DoesNotExist:
+        raise Http404("Category does not exist")
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+
+    paginator = Paginator(blog_posts, 5)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/posts.html', {'posts': posts, 'categories': categories})
