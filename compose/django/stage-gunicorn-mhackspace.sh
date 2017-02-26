892f34802354045accb0761a4a4ae42c63132ab4
Oliver Marks <oly@digitaloctave.com>
2017-02-26 11:32:28 +0000

    Fix typo

--- compose/django/stage-gunicorn-mhackspace.sh
+++ compose/django/stage-gunicorn-mhackspace.sh
@@ -2,4 +2,4 @@
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilescss
 
-/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/stage-gunicorn-mhackpace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
+/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/stage-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
