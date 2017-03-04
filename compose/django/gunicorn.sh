f590e03f521432ca97ea2e5d1bb6cf0119f16c7e
Oliver Marks <oly@digitaloctave.com>
2017-03-04 11:48:03 +0000

    reduce number of workers for stage to save the rams

--- compose/django/gunicorn.sh
+++ compose/django/gunicorn.sh
@@ -2,4 +2,4 @@
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilescss
 
-/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/gunicorn.sock --error-logfile /var/log/gunicorn/gunicorn-error.log --chdir=/app
+/usr/local/bin/gunicorn config.wsgi -w 3 -b unix:/data/sockets/gunicorn.sock --error-logfile /var/log/gunicorn/gunicorn-error.log --chdir=/app
--- compose/django/stage-gunicorn-mhackspace.sh
+++ compose/django/stage-gunicorn-mhackspace.sh
@@ -3,4 +3,4 @@ rm -R /app/staticfiles/*
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilescss
 
-/usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/stage-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
+/usr/local/bin/gunicorn config.wsgi -w 2 -b unix:/data/sockets/stage-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
