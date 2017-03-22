fcb493d2e34625f2ab974531a3165844cbb5e1f8
Sam Collins <sam@scollins.co>
2017-03-22 21:21:11 +0000

    Made published date editable

--- mhackspace/blog/admin.py
+++ mhackspace/blog/admin.py
@@ -6,18 +6,19 @@ from draceditor.models import DraceditorField
 
 from mhackspace.blog.models import Post, Category
 
+
 @admin.register(Post)
 class PostAdmin(ModelAdmin):
     date_hierarchy = 'published_date'
-    list_display = ('title', 'slug', 'author','active', 'members_only', 'published_date', 'updated_date')
+    list_display = ('title', 'slug', 'author', 'active', 'members_only', 'published_date', 'updated_date')
     list_filter = ('author', 'categories', 'members_only')
     search_fields = ('title', 'author__name', 'author__id', 'published_date', 'updated_date')
-    readonly_fields = ('published_date',)
     filter_horizontal = ('categories',)
     prepopulated_fields = {"slug": ("title",)}
     formfield_overrides = {
         DraceditorField: {'widget': AdminDraceditorWidget},
     }
+
 
 @admin.register(Category)
 class CategoryAdmin(ModelAdmin):
