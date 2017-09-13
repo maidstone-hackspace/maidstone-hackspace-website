8c9901947692484a4a9fd73e221a3a55ad430b6c
Sam Collins <sam@scollins.co>
2017-09-13 19:37:59 +0100

    Added back old password hashing algorithms

--- config/settings/common.py
+++ config/settings/common.py
@@ -333,12 +333,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # PASSWORD HASHING
-# ------------------------------------------------------------------------------
-# Use fast password hasher so tests run faster
-PASSWORD_HASHERS = (
+PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
 
 # AUTHENTICATION CONFIGURATION
 # ------------------------------------------------------------------------------
