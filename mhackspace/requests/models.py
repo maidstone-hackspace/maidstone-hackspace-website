833b08980a2fd9eaed831f3c7ff40bfea14edd9c
Oliver Marks <oly@digitaloctave.com>
2017-09-19 21:24:13 +0100

    Increase decimal places on request form

--- mhackspace/requests/models.py
+++ mhackspace/requests/models.py
@@ -21,7 +21,7 @@ class UserRequests(models.Model):
     title = models.CharField(max_length=255, help_text='Whats being requested ?')
     request_type = models.IntegerField(choices=REQUEST_TYPES, null=False)
     cost = models.DecimalField(
-        max_digits=4,
+        max_digits=6,
         decimal_places=2,
         help_text='Leave blank, if no associated cost, or add estimated cost if not sure.'
     )
