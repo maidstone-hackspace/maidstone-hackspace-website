afdb834daf4302c947a63293f731abb0eceb5d2b
Oliver Marks <oly@digitaloctave.com>
2018-01-06 21:11:58 +0000

    Disable django wiki login pages and use the default django pages

--- config/settings/common.py
+++ config/settings/common.py
@@ -369,6 +369,7 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 #LOGIN_URL = 'account_login'
 WIKI_SIGNUP_URL = 'account_login'
+WIKI_ACCOUNT_HANDLING = False
 #WIKI_EDITOR = 'wiki.editors.martor.Martor'
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
