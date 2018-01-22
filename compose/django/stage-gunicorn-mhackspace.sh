84db2d43d9d7ddca91a66450c9459ec9ef69fdac
Oliver Marks <oly@digitaloctave.com>
2018-01-22 21:11:09 +0000

    Add useful commands to readme, chane ownership of log file

--- compose/django/stage-gunicorn-mhackspace.sh
+++ compose/django/stage-gunicorn-mhackspace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm -R /app/staticfiles/*
 python /app/manage.py collectstatic --noinput
 python /app/manage.py compilescss
+chmod -R 664 /var/log/gunicorn
 
 /usr/local/bin/gunicorn config.wsgi -w 2 -b unix:/data/sockets/stage-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
