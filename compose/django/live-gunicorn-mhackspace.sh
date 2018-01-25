70589679a651246fe62a3f1e3cec5b2e80d748dc
Oliver Marks <oly@digitaloctave.com>
2018-01-25 21:45:13 +0000

    Updated deploy steps

--- compose/django/live-gunicorn-mhackspace.sh
+++ compose/django/live-gunicorn-mhackspace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm -R /app/staticfiles/*
-python /app/manage.py collectstatic --noinput
-python /app/manage.py compilescss
+#rm -R /app/staticfiles/*
+#python /app/manage.py collectstatic --noinput
+#python /app/manage.py compilescss
 
 /usr/local/bin/gunicorn config.wsgi -w 4 -b unix:/data/sockets/live-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/live-gunicorn-mhackspace.log --chdir=/app
--- compose/django/stage-gunicorn-mhackspace.sh
+++ compose/django/stage-gunicorn-mhackspace.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python /app/manage.py collectstatic --noinput
-python /app/manage.py compilescss
+#python /app/manage.py collectstatic --noinput
+#python /app/manage.py compilescss
 
 /usr/local/bin/gunicorn config.wsgi -w 2 -b unix:/data/sockets/stage-gunicorn-mhackspace.sock --error-logfile /var/log/gunicorn/stage-gunicorn-mhackspace.log --chdir=/app
