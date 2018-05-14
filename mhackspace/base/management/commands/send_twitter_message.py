cc2bea2d872f2b655a9c15032dac3467dd3eecd4
Oly <oly@digitaloctave.com>
2018-05-14 09:08:00 +0100

    fix import

--- mhackspace/base/management/commands/send_twitter_message.py
+++ mhackspace/base/management/commands/send_twitter_message.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-from mhackspace.core.tasks import twitter_message
+from mhackspace.base.tasks import twitter_message
 
 
 class Command(BaseCommand):
