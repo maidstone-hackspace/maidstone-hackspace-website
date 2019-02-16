370b0d7b2db9a529a1e39b92634b46e26c6831dc
Oliver Marks <oly@digitaloctave.com>
2019-02-16 14:28:51 +0000

    Handle missing image in sites blog.

--- mhackspace/base/feeds.py
+++ mhackspace/base/feeds.py
@@ -7,27 +7,31 @@ from mhackspace.feeds.models import Article
 class MediaRssFeed(Rss201rev2Feed):
     """
     Implement thumbnails which adhere to Yahoo Media RSS (mrss) feed.
-  
+
     @see http://djangosnippets.org/snippets/1648/
     """
+
     def rss_attributes(self):
         attrs = super(MediaRssFeed, self).rss_attributes()
-        attrs['xmlns:dc'] = "http://purl.org/dc/elements/1.1/"
-        attrs['xmlns:media'] = 'http://search.yahoo.com/mrss/'
+        attrs["xmlns:dc"] = "http://purl.org/dc/elements/1.1/"
+        attrs["xmlns:media"] = "http://search.yahoo.com/mrss/"
         return attrs
 
     def add_item_elements(self, handler, item):
         super(MediaRssFeed, self).add_item_elements(handler, item)
 
-        thumbnail = {'url': item['thumbnail_url']}
+        if item.get("thumbnail_url") is None:
+            return
 
-        if 'thumbnail_width' in item:
-            thumbnail['width'] = str(item['thumbnail_width'])
+        thumbnail = {"url": item["thumbnail_url"]}
 
-        if 'thumbnail_height' in item:
-            thumbnail['height'] = str(item['thumbnail_height'])
+        if "thumbnail_width" in item:
+            thumbnail["width"] = str(item["thumbnail_width"])
 
-        handler.addQuickElement(u"media:thumbnail", '', thumbnail)
+        if "thumbnail_height" in item:
+            thumbnail["height"] = str(item["thumbnail_height"])
+
+        handler.addQuickElement(u"media:thumbnail", "", thumbnail)
 
 
 class LatestEntriesFeed(Feed):
