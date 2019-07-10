25d52fea55da12e01c2efeec829afa81d902c3c5
Oliver Marks <oly@digitaloctave.com>
2019-07-10 18:41:54 +0100

    Format  status page.

--- mhackspace/core/views.py
+++ mhackspace/core/views.py
@@ -18,10 +18,11 @@ class StatusView(TemplateView):
     template_name = "partials/status.html"
 
     def get_context_data(self, *args, **kwargs):
-        membership_count = Membership.objects.all().count()
+        membership_count = Membership.objects.filter(status=1).all().count()
+        membership_expired = Membership.objects.filter(status=3).all().count()
         context = super().get_context_data(*args, **kwargs)
         context["members"] = membership_count
-        context["atspace"] = membership_count
+        context["members_expired"] = membership_expired
         return context
 
 
